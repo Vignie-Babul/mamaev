acbe4ce86b51d086c5e441df67d923780eb62a8e
Vignie Babul <vigniebabul.dev@gmail.com>
2026-03-25 11:34:58 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// run without `ConsoleApp5.csproj`: `dotnet run -p:AllowUnsafeBlocks=true Program.cs` (run with unsafe flag). 
+// run with    `ConsoleApp5.csproj`: `dotnet run Program.cs`                           (unsafe flag in the .csproj)
+
+
+using System;
 using System.Threading.Tasks;
 
 using ClassLibrary2;
@@ -14,31 +18,40 @@ unsafe class Program
 		const string task = "Fpk3crp6SqKyu7RBCYRdVJFl+53cJqqMxJeJHuDyVKfvqVQcC1kh4Gq0LzepYdfQdKidpTT9DaewWfy4nd5Dow8xp7Ub7O+VmCRFAhAGWJoWxvg0yBkxfBxMw9Qgn9w2umJ/V2GU/6IwO9NVT0IzLcYWYzsJcfe3wFd8Hi8MCbntqFtkIT7TT/rCFfsD8hTO";
 
 		const uint N_26_POW_4 = 456_976;
+
 		const byte LEN = 26;
 		const ushort LEN_POW_2 = 676;
 		const ushort LEN_POW_3 = 17_576;
-		const byte UNI_A = 65;
+
+		const byte UNICODE_A = 65;
 		const byte BUFFSIZE = 4;
+
+		string result_key = "\0";
+		string result_task = "\0";
 
 		Parallel.For(0, N_26_POW_4, new ParallelOptions {MaxDegreeOfParallelism = threads}, (i, state) => {
 			char* buff = stackalloc char[BUFFSIZE];
 
-			buff[3] = (char)(i%LEN + UNI_A);
-			buff[2] = (char)(i/LEN%LEN + UNI_A);
-			buff[1] = (char)(i/LEN_POW_2%LEN + UNI_A);
-			buff[0] = (char)(i/LEN_POW_3 + UNI_A);
+			buff[3] = (char)(i%LEN + UNICODE_A);
+			buff[2] = (char)(i/LEN%LEN + UNICODE_A);
+			buff[1] = (char)(i/LEN_POW_2%LEN + UNICODE_A);
+			buff[0] = (char)(i/LEN_POW_3 + UNICODE_A);
 
 			string key = new string(buff, 0, BUFFSIZE);
 
 			Cryptor cryptor = new Cryptor();
 
-			if (cryptor.CorrectCode(task, key)) {
+			try {
 				string result = cryptor.Decoding(task, key);
 				if (result.Contains("РАСШИФРОВАННО")) {
 					Console.WriteLine($"key: {key}\ntext: {result}");
+					result_key = key;
+					result_task = result;
 					state.Stop();
 				}
-			}
+			} catch {}
 		});
+
+		Console.WriteLine($"\n\nkey: {result_key}\ntext: {result_task}");
 	}
 }
